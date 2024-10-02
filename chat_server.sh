0f5b1da00f0be29a0c95c8651b0515e1c7c04413
pawan <150579399+isafwck@users.noreply.github.com>
2024-10-02 18:35:45 +0530

    Update chat_server.sh

--- chat_server.sh
+++ chat_server.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 PORT=12345
-PIPE=$(mktemp -u)
-mkfifo $PIPE
+CLIENTS=()
+
+mkfifo chat_pipe
 
 {
     while true; do
-        cat $PIPE | nc -l -p $PORT
+        if read MESSAGE < chat_pipe; then
+            for CLIENT in "${CLIENTS[@]}"; do
+                echo "$MESSAGE" > "$CLIENT"
+            done
+        fi
     done
 } &
 
-echo "Chat server started on port $PORT."
-
 while true; do
-    read MESSAGE
-    echo "$MESSAGE" > $PIPE
+    nc -l -p $PORT | {
+        read USERNAME
+        CLIENT_FD=$(tty)
+        CLIENTS+=("$CLIENT_FD")
+        echo "User $USERNAME has joined the chat."
+
+        while read LINE; do
+            echo "$USERNAME: $LINE" > chat_pipe
+        done
+
+        CLIENTS=("${CLIENTS[@]/$CLIENT_FD}")
+        echo "User $USERNAME has left the chat."
+    }
 done
